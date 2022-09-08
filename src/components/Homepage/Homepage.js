c8ddf9d5253b7cddad6c5c31c87cdb7182887af2
olatundesleek <olatundesleek@gmail.com>
2022-09-08 06:55:32 -0700

    initial homepage commit

--- src/components/Homepage/Homepage.js
+++ src/components/Homepage/Homepage.js
@@ -1,24 +1,61 @@
-import Link from "next/link"
+import Link from "next/link";
 // import link from "next/link"
-
+import logoImage from "assets/images/tdclogo.png";
 function Homepage() {
   return (
     <div>
-        
-        <section class="showcase">
+      <section class="showcase">
         <nav className="homepage-nav">
-<Link href=""><a className="homepage-nav-a">TheDoggCoin.com</a></Link>
+          <Link href="">
+            <a className="homepage-nav-a">
+              <img className="logo-image" src={logoImage} />
+            </a>
+          </Link>
         </nav>
-    <div class="video-container">
-        <video src="/video/snoopbackground.mp4" autoPlay loop></video>
+        <div class="video-container">
+          <video src="/video/snoopbackground.webm" autoPlay loop></video>
+        </div>
+        <div class="content">
+          <Link href="presale">
+            <button
+              className="main-btn main-btn-1 btn-block btn-lg swapbtn"
+              metamask-switch-network-button=""
+            >
+              <span className="swapspan"></span>
+              <span className="swapspan"></span>
+              <span className="swapspan"></span>
+              <span className="swapspan"></span>
+              ROAD MAP
+            </button>
+          </Link>
+          <Link href="presale">
+            <button
+              className="main-btn main-btn-1 btn-block btn-lg swapbtn"
+              metamask-switch-network-button=""
+            >
+              <span className="swapspan"></span>
+              <span className="swapspan"></span>
+              <span className="swapspan"></span>
+              <span className="swapspan"></span>
+             TOKENOMICS
+            </button>
+          </Link>
+          <Link href="presale">
+            <button
+              className="main-btn main-btn-1 btn-block btn-lg swapbtn"
+              metamask-switch-network-button=""
+            >
+              <span className="swapspan"></span>
+              <span className="swapspan"></span>
+              <span className="swapspan"></span>
+              <span className="swapspan"></span>
+              BUY PRESALE
+            </button>
+          </Link>
+        </div>
+      </section>{" "}
     </div>
-    <div class="content">
-        <h1>Shoot For The Stars</h1>
-        <h3>Full screen video landing page</h3>
-        <a href="#about" class="btn">Read More</a>
-    </div>
-</section> </div>
-  )
+  );
 }
 
-export default Homepage
+export default Homepage;
